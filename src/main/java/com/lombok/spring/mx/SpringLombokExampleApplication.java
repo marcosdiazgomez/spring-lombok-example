438a19a9bb08779c2c2d9ab448ac166dce5f599e
MarcosIvanDiazGomez <marcosdiazgomez@gamil.com>
2024-01-17 08:30:52 -0600

    Add elements whit create project. --file whit config spring. --file maven. --file proyect.

---
+++ src/main/java/com/lombok/spring/mx/SpringLombokExampleApplication.java
@@ -0,0 +1,13 @@
+package com.lombok.spring.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLombokExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLombokExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lombok/spring/mx/SpringLombokExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lombok.spring.mx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLombokExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
